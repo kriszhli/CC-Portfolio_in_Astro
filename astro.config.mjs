79c9de8778a8989530e10f94e882f31d777d16a9
kris <tboxmonster@gmail.com>
2026-04-10 22:46:21 -0500

    Fix asset paths for GitHub Pages sub-directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://kriszhli.github.io/CC-Portfolio_in_Astro',
+  site: 'https://kriszhli.github.io',
+  base: '/CC-Portfolio_in_Astro',
   integrations: [],
 });
