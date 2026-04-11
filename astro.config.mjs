bd99fa8288f90817f1a90cfc6488b5867f5f63d9
kris <tboxmonster@gmail.com>
2026-04-10 22:39:45 -0500

    Update astro.config.mjs and deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://kriszhli.github.io',
-  base: '/CC-Portfolio_in_Astro',
+  output: 'static',
+  site: 'https://kriszhli.github.io/CC-Portfolio_in_Astro',
+  integrations: [],
 });
